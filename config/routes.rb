374ada5331f7de95980ff7d09493bc67ca2c4aa4
AsoYusuke <aso.aso.1227@i.softbank.jp>
2021-04-16 22:50:51 +0000

    ジャンル追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+  namespace :user do
+    resources :products
+  end
+
   devise_for :admins
+  namespace :admin do
+    resources :products
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+   
+
+
 end
